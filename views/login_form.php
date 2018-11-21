6d26b26859507044855e6b5b9e24a4cfe7a0459c
Al Kanngo <aladdin.kanngo@hotmail.com>
2018-11-21 18:41:39 +0100

    Added card-titel and card-link

--- views/login_form.php
+++ views/login_form.php
@@ -6,40 +6,49 @@
 ?>
 
 <body>
-<main class="container">
 
-    <form action="includes/login.php" method="post" name="form">
+    <main class="container">
 
-            <div class="col">
+        <div class="card text-center">
+
+            <h5 class="card-title">Login</h5>
+
+            <form action="includes/login.php" method="post" name="form">
 
                 <div class="col-md-6 col-sm-6 col-xs-6 form-group">
 
                     <label for="login_username">Username</label>
-                    <input type="text" class="form-control" name="login_username" id="login_username"
-                        placeholder="Your Username" required>
+                    <input type="text" class="form-control" name="login_username" id="login_username" placeholder="Your Username"
+                        required>
 
                 </div>
 
                 <div class="col-md-6 col-sm-6 col-xs-6 form-group">
 
                     <label for="login_password">Password</label>
-                    <input type="password" class="form-control" name="login_password" id="login_password"
-                        placeholder="Your Password" required>
+                    <input type="password" class="form-control" name="login_password" id="login_password" placeholder="Your Password"
+                        required>
 
                 </div>
 
                 <div class="col-md-6 col-sm-6 col-xs-6 form-group text-center">
 
-                    <input type="submit" value="Login" class="btn btn-success">
+                    <input type="submit" value="Login" class="btn">
 
                 </div>
-            </div>
-    </form>
-</main>
+
+            </form>
+
+            <a href="#" class="card-link">New? Sign up!</a>
+
+        </div>
+
+    </main>
 
 <?php
     require "../includes/footer.php"
 ?>
-    
+
 </body>
+
 </html>
