689cc28479ad903acb5d75f55693765f3a70e920
Ahnna <ahnnaostling@gmail.com>
2018-11-21 14:01:04 +0100

    This commit will add files changes, read body
    This commit will creat _footer.scss, _single-post.scss, and single-post.php, also adds read more button to footer and changes location of style.css in head

--- includes/footer.php
+++ includes/footer.php
@@ -1,9 +1,20 @@
-    <footer>
-        <div class="col-6">
-            <h3>Join the Club</h3>
-                <p>Sign up now and get 10% off in-store or online +
-                always free delivery!
-                </p>
+    <footer class="container">
+        <div class="row">
+            <div class="col-6">
+                
+                <h3>Join the Club</h3>
+                    <p>Sign up now and get 10% off in-store or online +
+                    always free delivery!
+                    </p>
+
+            </div>
+
+            <div class="col-6">
+                
+                <form>
+                    <input type="submit" value="Read More">
+                </form>
+            </div>
         </div>
     </footer>
-</body>>
+</body>
---
+++ views/single-post.php
@@ -0,0 +1,16 @@
+<?php
+    include '../includes/head.php';
+?>
+
+<main class="container">
+    <div class="row">
+        <div class="col-12">
+            <h2>Latest Article</h2>
+        </div>
+
+</main>
+
+<?php
+    include '../includes/footer.php';
+?>    
+
