5d572784f754783ed4105972d89b5068b8eee3a7
KaoriYoshimura <infmalbyevo@gmail.com>
2018-11-21 15:46:43 +0100

    Updating for register page
    This commit will update head.php in the includes folder, _register.scss file and style.scss in the scss folder, register.php in the views folder for style change.

--- includes/head.php
+++ includes/head.php
@@ -6,5 +6,5 @@
     <title>Ahnna's Shop</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans" rel="stylesheet">
-    <link rel="stylesheet" href="css/stylesheet.css">
+    <link rel="stylesheet" href="../css/style.css">
 </head>
--- views/register.php
+++ views/register.php
@@ -19,69 +19,40 @@ if(isset($_SESSION["username"])){
 
 
 <body>
-    <main id="login"> 
+    <main id="register"> 
 
         <div class="container">
             <div class="contents">
 
-                <p>To shop our products, please register and log in.</p>
                 <!--  Register form  -->
-                <h2>Register</h2>
+                <h1>Sign up</h1>
 
-                <p>if you are not registered, please register first.</p>
-
-                <form action="../includes/register_admin.php" method="post" id="form_register">
+                <form action="../includes/register.php" method="post" id="form_register">
                     <div class="form-group">
                         <label for="username">User Name</label>
-                        <input type="text" class="form-control" id="name" name="username" placeholder="Enter name">
-                        <small class="form-text text-muted">Please enter User Name.</small>
-                        <label for="email">Email address</label>
-                        <input type="email" class="form-control" id="email" name="email" placeholder="Enter email address">
-                        <small class="form-text text-muted">We'll never share your email with anyone else.</small>
+                        <input type="text" class="form-control" id="name" name="username" placeholder="Your Username">
+                        <small class="form-text">Please enter User Name.</small>
+                        <label for="email">Email</label>
+                        <input type="email" class="form-control" id="email" name="email" placeholder="Your email">
+                        <small class="form-text">We'll never share your email with anyone else.</small>
                         <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
-                        <small class="form-text text-muted">Password must be longer than 10 charactors.</small>
+                        <input type="password" class="form-control" id="password" name="password" placeholder="Your password">
+                        <small class="form-text">Password must be longer than 10 charactors.</small>
                     </div>
-                    <button type="submit" class="btn button_color">Register</button>
+                    <button type="submit" class="btn button-color">SIGN UP</button>
                 </form>
+                
+                <p>Already a member? Log in!</p>
 
                 <!--  Display error message  if register is unsucceeded
 If any of input forms are missing, redirect to the top of this page and show the error message.
 Otherwise redirect to index.php top.-->
                 <?php
-                is_error("empty_error", "error", "exist_error", null);
-                is_successfull("register", null, null, null);
+                //is_error("empty_error", "error", "exist_error", null);
+                //is_successfull("register", null, null, null);
 
                 ?>          
 
-                <!--  Devider       -->
-
-                <hr>
-
-                <!--  Log in  form       -->
-
-                <h2>Log in</h2>
-
-                <p>Please log in.</p>
-                <form action="../../private/partials/login_admin.php" method="post" id="form_login">
-                    <div class="form-group" id="login">
-                        <label for="username">Name</label>
-                        <input type="text" class="form-control" id="name" name="username" placeholder="Enter name">
-                        <small class="form-text text-muted">Please enter username.</small>
-                        <label for="password">Password</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
-                        <small class="form-text text-muted">Please enter your password</small>
-
-                    </div>
-                    <button type="submit" class="btn button_color">Log in</button>
-                </form>                
-                <!--  Display error message  if log in is unsucceeded
-If any of input forms are missing redirect to index.php top and show the error message.
-Otherwise redirect to index.php top  and show the error message.
--->
-                <?php
-                is_error("log_empty_error", "log_error", "pw_error", null)
-                ?>
 
 
             </div>
