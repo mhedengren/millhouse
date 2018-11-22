a08320e67104c4b12256c507d652d901e13cbdb9
mhedengren <melker.hedengren@medieinstitutet.se>
2018-11-22 16:27:49 +0100

    super minor structure change

--- index.php
+++ index.php
@@ -9,9 +9,7 @@ $page_title = 'Index';
 
 <?php include 'includes/head.php'; ?>
 
-<?php
-include 'includes/header.php';
-?>
+<?php include 'includes/header.php';?>
     
     
 <script src="main.js"></script>
