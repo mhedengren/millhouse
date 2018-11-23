de456eeff8bd5c6a5573447a568c9d16923cf121
Ahnna <ahnnaostling@gmail.com>
2018-11-23 14:12:24 +0100

    This commit will update work in single-post and fixinf database

--- views/single-post.php
+++ views/single-post.php
@@ -4,18 +4,21 @@
     //Defines page title
     $page_title = "single-post";
     include '../includes/head.php';
-    include '../includes/header.php';   
+    include '../includes/header.php';
+    include '../includes/database-connection.php';   
 ?>
 
 <main class="container">
     <div class="row">
         <div class="col-12">
             <img src="../images/hero-image.jpg" alt="woman's body in floral dress" class="single-post-image">
-            <h2 class="article-title"><span>How we dress for the season's best parties<span></h2>
+            <h2 class="article-title">How we dress for the season's best parties</h2>
             <br>
-                <h4 class="article-date"><span>November 22, 2018<span></h4>
+                <h4 class="article-date">November 22, 2018</h4>
         </div>
-
+    <div class="row justify-content-center">    
+        <div class="col-12">
+    
 </main>
 
 <?php
