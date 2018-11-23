b3306f389d29c25670c34931bfecdde63264bbca
mhedengren <melker.hedengren@medieinstitutet.se>
2018-11-23 20:55:23 +0100

    single post and feature styling complete

--- index.php
+++ index.php
@@ -22,11 +22,13 @@ $page_title = 'Index';
                             <div class="hero-image">
                             <img src="<?= $post["image"]; ?>" alt="Feature-image">
                             </div>
-                            <div class="date-circle">
-                            <h6 class=""><?= substr($post["created_on"], 8, 15); ?></h6>
+                            <div class="date row justify-content-center">
+                                <div class="date-circle">
+                                <h6 class=""><?= substr($post["created_on"], 8, 15); ?></h6>
+                                </div>
                             </div>
                             <h2 class="post-title"><?= $post["title"]; ?></h2>
-                            <p class="post-description"><?= substr($post["description"], 0, 400); ?></p>
+                            <p class="post-description"><?= substr($post["description"], 0, 150); ?></p>
                     </div>
                     <?php endforeach; ?>         
     </div>
