3a57aee451c787faf345ca30f472dfc9d16ad0c8
Ahnna <ahnnaostling@gmail.com>
2018-11-23 09:48:32 +0100

    This commit will remove database connection from git

--- includes/database-connection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$database_host = 'mysql:host=localhost:3306;dbname=millhouse;charset=utf8';
-$database_username = 'root';
-$database_password = 'root';
-
-$pdo = new PDO($database_host, $database_username, $database_password);
-// Display all query errors that occur, otherwise these are muted
-$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
