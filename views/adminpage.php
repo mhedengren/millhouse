a905726412b0196965dbc56793f7edca273afb26
Cristina <cris.barbagallo@gmail.com>
2018-11-23 12:43:28 +0100

    Add form input text-area

--- views/adminpage.php
+++ views/adminpage.php
@@ -1,4 +1,3 @@
-
 <?php
 //Define the siteroot for includes/requires
 $siteroot = "..";
@@ -13,32 +12,37 @@ include '../includes/header.php';
 
 <body>
 
- <main id="admin-panel">
+    <main id="admin-panel">
 
-  <div class="container">
+        <div class="container">
 
-   <div class="row">
-        <div class="col-sm">
-     <h1>Admin Panel</h1>
-    </div>
-   </div>
-   
-   <div class="row">
-    <div class="col-sm-12 col-md-8">
-     <h2>Add New Post</h2>
-     <form action="">
-      <div class="form-group">
-       <label for="post-title">Title</label>
-       <input type="text" class="form-control" id="post-title" placeholder="Enter title here">
-      </div>
-     </form>
-    </div>
-   </div>
+            <div class="row">
+                <div class="col-sm">
+                    <h1>Admin Panel</h1>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-sm-12 col-md-8">
+                    <h2>Add New Post</h2>
+                    <form action="#" method="POST">
+                        <div class="form-group">
+                            <label for="post-title">Title</label>
+                            <input type="text" class="form-control" id="post-title" placeholder="Enter title here">
+                        </div>
+                        <div class="form-group">
+                            <label for="text-area">Body</label>
+                            <textarea class="form-control" id="text-area" rows="15" placeholder="Enter text here"></textarea>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
 
-  </div>
-  
- </main>
+        </div>
+
+    </main>
 
 </body>
+
 </html>
