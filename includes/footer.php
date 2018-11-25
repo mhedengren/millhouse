1e9d45f24e029e735f4265d29d067a94f55b0a5c
mhedengren <melker.hedengren@medieinstitutet.se>
2018-11-25 13:34:52 +0100

    fix some minor indent

--- includes/footer.php
+++ includes/footer.php
@@ -1,11 +1,11 @@
     <footer class="container-fluid">
-        <div class="row justify-content-around text-center">
-            <div class="col-4">
+        <div class="row text-center">
+            <div class="col-6">
                 <h3>Join the Club</h3>
                     <p>Sign up now and get 10% off in-store or online +
                     always free delivery!</p>
             </div>
-            <div class="col-4">
+            <div class="col-6">
                <a href="#">Read more</a>
             </div>
      
@@ -13,19 +13,6 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-    <script>
-window.onscroll = function() {myFunction()};
 
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-</script>
 </body>
 </html>
--- index.php
+++ index.php
@@ -14,11 +14,11 @@ $page_title = 'Index';
 
 <main id="index-page">
     <div class="container">
-        <article class="row feature-post">
+        <article class="row justify-content-between feature-post">
             <?php
             //Looping only one post from db. (LIMIT 1)
             foreach ($all_posts as $post): ?>
-                <div class="col-sm-12">
+                <div class="col-sm-12 col-md-8">
                     <div class="hero-image">
                     <img src="<?= $post["image"]; ?>" alt="feature-image">
                     </div>
@@ -30,8 +30,13 @@ $page_title = 'Index';
                     <h2 class="post-title"><?= $post["title"]; ?></h2>
                     <p class="post-description"><?= substr($post["description"], 0, 150); ?></p>
                 </div>
-            <?php endforeach; ?>  
-        </article>       
+            <?php endforeach; ?> 
+            
+            <div class="col-xs-12 col-lg-4">
+            <h1>Dummy column text test</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet saepe fugiat sunt voluptatibus error sequi perferendis iure a aliquid illum nostrum quisquam odit cum neque culpa delectus illo nemo officiis pariatur nihil, obcaecati, voluptas esse temporibus! At harum ullam eveniet.</p></div>  
+        </article>     
+        
     </div>
 </main>
 
