05c7107be0eedc09bc5117bb8f43274f2157d387
Cristina <cris.barbagallo@gmail.com>
2018-11-26 19:25:13 +0100

    Fix form code for image and text editor

--- views/admin-page.php
+++ views/admin-page.php
@@ -12,45 +12,60 @@ include '../includes/header.php';
 
 <body>
 
-    <?php
-  include 'partials/fetch_images.php';
-  foreach($all_images as $image): ?>
-    <div>
-      <img src="<?= $image["image"]; ?>" alt="Cool image.">
-      <div>
-        <?= $image["text"]; ?>
-      </div>
-    </div>
-  <?php
-  endforeach;
-  ?>
+    <main id="admin-panel">
 
-  <main>
-    <!-- If we are sending a file in a form we must supply the extra attribute
-         'encytype="multipart/form-data"', otherwise the file will be sent as a
-         string and not uploaded to the server, otherwise the form is similar to every other form -->
-    <form action="upload.php" method="POST" enctype="multipart/form-data">
-      <label for="image">Image</label>
-      <!-- Use 'type="file"' to automatically create a input-field for uploads -->
-      <input type="file" name="image" id="image">
-      <label for="text">Image text</label>
-      <!-- Use a textarea for a bigger input-field, put an ID on the area for the
-           wysiwyg-editor to initialize on -->
-      <textarea name="text" id="text" ></textarea>
-      <input type="submit" value="Send">
-    </form>
-  </main>
+        <div class="container">
 
-  <!-- Include external JS libs. -->
+            <div class="row">
+                <div class="col-sm">
+                    <h1>Admin Panel</h1>
+                </div>
+            </div>
+
+
+
+            <div class="row new-post">
+                <div class="col-sm-12 col-md-8">
+                    <h2 class="add-new-post">Add New Post</h2>
+                    <!-- If we are sending a file in a form we must supply the extra attribute
+                    'encytype="multipart/form-data"', otherwise the file will be sent as a
+                    string and not uploaded to the server, otherwise the form is similar to every other form -->
+                    <form action="../includes/upload.php" method="POST" enctype="multipart/form-data" id="upload-form">
+                        <!-- Use a textarea for a bigger input-field, put an ID on the area for the
+                        wysiwyg-editor to initialize on -->
+                        <textarea name="text" id="text"></textarea>
+                        <input type="submit" value="Send">
+                    </form>
+            </div>
+            <div class="col-sm-12 col-md-4">
+                <div class="upload-image-form">
+                    <h3 class="featured-image">Featured Image</h3>
+                    <h4 class="select-image">Select Image to Upload:</h4>
+                    <input type="file" name="fileToUpload" id="fileToUpload" for="upload-form">
+                    <input type="submit" name="submit" value="Upload Image" for="upload-form">
+                </div>
+            </div>
+
+        </div>
+
+
+        </div>
+
+    </main>
+
+    <!-- Include external JS libs. -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.js"></script>
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/mode/xml/xml.min.js"></script>
- 
+
     <!-- Include Editor JS files. -->
     <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@2.9.1/js/froala_editor.pkgd.min.js"></script>
- 
+
     <!-- Initialize the editor. -->
-    <script> $(function() { $('textarea').froalaEditor() }); </script>
+    <script>
+        $(function () {
+            $('textarea').froalaEditor()
+        });
+    </script>
 
-
-<?php include '../includes/footer.php'; ?>
+    <?php //include '../includes/footer.php'; ?>
