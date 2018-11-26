1fbfd89564f91dbd0b6dcd5b3fa43572401e55f7
mhedengren <melker.hedengren@medieinstitutet.se>
2018-11-26 10:16:56 +0100

    remove test sidebarthing

--- index.php
+++ index.php
@@ -14,11 +14,11 @@ $page_title = 'Index';
 
 <main id="index-page">
     <div class="container">
-        <article class="row justify-content-between feature-post">
+        <article class="row feature-post">
             <?php
             //Looping only one post from db. (LIMIT 1)
             foreach ($all_posts as $post): ?>
-                <div class="col-sm-12 col-md-8">
+                <div class="col-sm-12 col-md-12">
                     <div class="hero-image">
                     <img src="<?= $post["image"]; ?>" alt="feature-image">
                     </div>
@@ -31,12 +31,7 @@ $page_title = 'Index';
                     <p class="post-description"><?= substr($post["description"], 0, 150); ?></p>
                 </div>
             <?php endforeach; ?> 
-            
-            <div class="col-xs-12 col-md-4">
-            <h1>Dummy column text test</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint, eveniet saepe fugiat sunt voluptatibus error sequi perferendis iure a aliquid illum nostrum quisquam odit cum neque culpa delectus illo nemo officiis pariatur nihil, obcaecati, voluptas esse temporibus! At harum ullam eveniet.</p></div>  
         </article>     
-        
     </div>
 </main>
 
