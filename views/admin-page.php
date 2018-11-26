f6be2bc614bc9e9e8d6b1bc7f0c394a117c7d987
Cristina <cris.barbagallo@gmail.com>
2018-11-26 18:49:30 +0100

    Add link dependencies for the editor

--- views/admin-page.php
+++ views/admin-page.php
@@ -69,6 +69,12 @@ include '../includes/header.php';
 
     </main>
 
+  <!-- Link dependencies for the editor -->
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
+  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> 
+  <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
+  <script>
+
     <script>
         $(document).ready(function () {
             $('#summernote').summernote();
