09dd22e521d30b9883a4f30db81edffca129aad1
Cristina <cris.barbagallo@gmail.com>
2018-11-26 19:13:57 +0100

    Add scripts from Froala Editor

--- views/admin-page.php
+++ views/admin-page.php
@@ -41,17 +41,16 @@ include '../includes/header.php';
     </form>
   </main>
 
-  <!-- Link dependencies for the editor -->
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script> 
-  <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script> 
-  <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.11/summernote.js"></script>
-  <script>
-    /**
-     * use the id of the textarea in the form to initialize this text-editor: #text
-     */
-    $(document).ready(function() {
-      $('#text').summernote();
-    });
-  </script>
+  <!-- Include external JS libs. -->
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.js"></script>
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/mode/xml/xml.min.js"></script>
+ 
+    <!-- Include Editor JS files. -->
+    <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/froala-editor@2.9.1/js/froala_editor.pkgd.min.js"></script>
+ 
+    <!-- Initialize the editor. -->
+    <script> $(function() { $('textarea').froalaEditor() }); </script>
 
-    <?php include '../includes/footer.php'; ?>
+
+<?php include '../includes/footer.php'; ?>
