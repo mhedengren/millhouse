80f4a52e19feab6889f9cfa27c6094f3b59a447e
Cristina <cris.barbagallo@gmail.com>
2018-11-28 14:07:36 +0100

    Delete error handling code

--- views/admin-page.php
+++ views/admin-page.php
@@ -1,7 +1,6 @@
 <?php
 //Start session
-session_start(); 
-ini_set(‘display_errors’, 1); ini_set(‘display_startup_errors’, 1); error_reporting(E_ALL);
+session_start();
 
 //Include for absolute path
 include '../config.php';
