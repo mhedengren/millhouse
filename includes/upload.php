05c4626f5cf660985f59237f06cf2fa52e2ce946
Cristina <cris.barbagallo@gmail.com>
2018-11-28 13:56:42 +0100

    create code to store information about the text editor form and the upload image

--- includes/upload.php
+++ includes/upload.php
@@ -1,90 +1,52 @@
 <?php
 require '../includes/database-connection.php';
 
-//var_dump($_POST["text"]);
+//var_dump($_POST["postTitle"]);
+//var_dump($_POST["postDesc"]);
 //var_dump($_FILES["image"]);
 
-/* We now process the form data:
-if form has been submitted then process it*/
 if(isset($_POST['submit'])){
-  //remove any slashed in the $_POST array
-  $_POST = array_map( 'stripslashes', $_POST );
 
-  //collect form data
-  extract($_POST);
+  //Retrieve the inputs values from the form
+  $postTitle = $_POST['postTitle'];
+  $postDesc = $_POST['postDesc'];
+  $created_by = $_SESSION['admin'];
+  $created_on = date('Y-m-d');
+  $image = $_FILES["image"];
 
-    /* any post element is then accessible 
-    by using just its name so $_POST['postTitle'] becomes $postTitle */
-    if($postTitle ==''){
-      $error[] = 'Please enter the title.';
+
+  $temporary_location = $image["tmp_name"];
+  $new_location = "uploads/" . $image["name"];
+  $upload_ok = move_uploaded_file($temporary_location, $new_location);
+
+  //Check if the inputs are empty
+  if(empty($_POST["postTitle"]) || empty($_POST["postDesc"])) {
+        header('Location: ../views/admin-page.php?action=empty');
+        exit();
     }
 
-    if($text ==''){
-      $error[] = 'Please enter the description.';
-    }
-    
-
-} else if(!isset($error)){
   //if no error has been set then insert the data into the database
-
+  if($upload_ok){
     try {
-        $stmt = $db->prepare('INSERT INTO posts (title, description, created_by, created_on, image) VALUES (:postTitle, :text, :admin, :postDate, :image)') ;
-        $stmt->execute(array(
+        $statement = $pdo->prepare(
+          'INSERT INTO posts (title, description, created_by, created_on, image) 
+          VALUES (:postTitle, :postDesc, :created_by, :postDate, :image)'
+          );
+
+        $statement->execute(array(
             ':postTitle' => $postTitle,
             ':postDesc' => $postDesc,
-            ':postDate' => date('Y-m-d H:i:s')
+            ':created_by' => $created_by,
+            ':postDate' => $created_on,
+            ":image" => $new_location
         ));
 
-        //redirect to index page
-        header('Location: index.php?action=added');
+        //redirect to admin page
+        header('Location: ../views/admin-page.php?action=added');
         exit;
 
     } catch(PDOException $e) {
         echo $e->getMessage();
     }
-
-}
-
-
-
-/**
- * If the file is sent via a form with 'enctype="multiplart/form-data"' the
- * file is saved go the superglobal '$_FILES' variable and inside of $_FILES["image"]
- */
-$image = $_FILES["image"];
-$image_text = $_POST["text"];
-$title = $_POST["title"];
-/**
- * When it is uploaded it is stored at a temporary location inside a /tmp folder
- * on your computer or server. We must move this temporary file to a permanent location
- * which we will do futher down. The path to the temporary location is inside of $_FILES["image"]["tmp_name"]
- */
-$temporary_location = $image["tmp_name"];
-/**
- * We must create a new filename and location and decide where we will put the uploaded file.
- * We can specify this new location as 'uploads/' (the name of the folder where we want our images)
- * and then reuse the name of the uploaded file. In my example the file is named 'tired.png' so the
- * new file will be named 'uploads/tired.png'
- */
-$new_location = "uploads/" . $image["name"];
-/**
- * 'move_uploaded_file' moves the file from the temporary location to your newly specified location
- * if the transfer is complete we will get a true/false return value from the function indiciating
- * everything went ok with the transfer of the file
- */
-$upload_ok = move_uploaded_file($temporary_location, $new_location);
-/**
- * If the transfer went OK we can insert the pathname to the image into the database, not the actual
- * file, the file is stored in a folder, and the path to the file is saved in the database as a VARCHAR
- * Here I am also sending along the text from the editor, that text is saved as usual in $_POST
- */
-if($upload_ok){
-  $statement = $pdo->prepare("INSERT INTO posts (image, description) VALUES (:image, :text)");
-  $statement->execute([
-    ":image" => $new_location,
-    ":text"  => $image_text
-  ]);
-  
-  //When everything is done, redirect
-  header('Location: views/admin-page.php');
+  }
 }
