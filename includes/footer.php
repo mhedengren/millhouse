34f3ccbfd8f63b549e91438c2128da05f85219f6
Ahnna <ahnnaostling@gmail.com>
2018-11-28 09:46:36 +0100

    This commit will add css classes and scss to footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
-    <footer class="container-fluid">
+    <footer class="container-fluid" id="footer">
         <div class="row d-flex align-items-center">
             <div class="col-md-6 text-center">
-                <h3>Join the Club</h3>
-                    <p>Sign up now and get 10% off in-store or online +
+                <h3 class="footer-title">Join the Club</h3>
+                    <p class="footer-p">Sign up now and get 10% off in-store or online +
                     always free delivery!</p>
             </div>
             <div class="col-md-6 text-center">
