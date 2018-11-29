90f7df6e5b9ffae028b9fe41ffeecc78f963a5cb
Cristina <cris.barbagallo@gmail.com>
2018-11-29 11:19:51 +0100

    Add function for display error messages when uploading a image

--- includes/functions.php
+++ includes/functions.php
@@ -36,10 +36,30 @@ function display_errors($errors = array()){
   the inputs field in the add new post form */
 function new_post_form_check() {
 
-    if($_GET['action'] == 'empty') {
-        echo "<div class='alert alert-danger text-center' role='alert'>You did not fill in all fields!</div>";
-    }
+ if(isset($_GET['empty'])) {   
+    if($_GET['empty'] == 'title') {
+        echo "<div class='alert alert-danger text-center' role='alert'>Please insert the title!</div>";
+    } else if($_GET['empty'] == 'description') {
+        echo "<div class='alert alert-danger text-center' role='alert'>Please insert the description!</div>";
+    } else if($_GET['empty'] == 'content') {
+        echo "<div class='alert alert-danger text-center' role='alert'>Please insert the content!</div>";
+    } else if($_GET['empty'] == 'form') {
+        echo "<div class='alert alert-danger text-center' role='alert'>You did not fill in all fields! </div>";
+    } 
+ } else if (isset($_GET['action'])) {
+     if($_GET['action'] == 'added')
+    echo "<div class='alert alert-success text-center' role='alert'>Your post has been successfully added!</div>";
+ }
 }
 
+function upload_file_check() {
+    if(isset($_GET['upload'])) {
+        if($_GET['upload'] == 'wrongformat') {
+            echo "<div class='alert alert-warning text-center' role='alert'>Fil extension not allowed, please choose a JPEG, JPG or PNG file.</div>";
+        } else if($_GET['upload'] == 'empty') {
+            echo "<div class='alert alert-warning text-center' role='alert'>Please choose an image to upload.</div>";
+        }
+    }
+} 
 
 ?>
