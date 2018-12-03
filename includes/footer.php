a1e46d12a69423b9b668ee3b0077391fdfc482f2
mhedengren <melker.hedengren@medieinstitutet.se>
2018-12-03 15:28:34 +0100

    will make footer fully visible

--- includes/footer.php
+++ includes/footer.php
@@ -1,15 +1,16 @@
 <footer class="container-fluid" id="footer">
-        <div class="row footer-color d-flex align-items-center">
-            <div class="col-md-6 text-center">
+   
+        <div class="row footer-color d-flex align-items-center justify-content-center">
+            <div class="col-sm-12 col-md-6 text-center">
                 <h3 class="footer-title">Join the Club</h3>
-                    <p class="footer-p">Sign up now and get 10% off in-store or online +
+                    <p class="footer-p">Sign up now and get 10% off in-store or online +<br>
                     always free delivery!</p>
             </div>
-            <div class="col-md-6 text-center">
+            <div class="col-sm-12 col-md-6 text-center">
                <a href="#" class="button-link">READ MORE</a>
             </div>
         </div>    
-     
+
     </footer>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
