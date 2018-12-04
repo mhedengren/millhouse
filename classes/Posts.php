db06ea3a3694ef62720d036591f1a2e9b7e464a3
mhedengren <melker.hedengren@medieinstitutet.se>
2018-12-04 15:51:03 +0100

    added properties for posts class

--- classes/Posts.php
+++ classes/Posts.php
@@ -2,14 +2,19 @@
 
 class Posts
 {
+
+  //property for pdo
   private $pdo;
-  
-  
 
+   //properties for inputting data;
+   public $content;
+   public $created_on;
+   public $created_by;
+   public $posts_id;
+   public $errors;
+  
   /* Inject the pdo connection so it is available inside of the class
-   * so we can call it with '$this->pdo', always available inside of the class
-   */
-
+   * so we can call it with '$this->pdo', always available inside of the class */
   public function __construct($pdo)
   {
     $this->pdo = $pdo;
