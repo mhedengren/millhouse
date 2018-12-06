34988f86df4a9c0f572c7ae9eb32f6667dc924eb
Cristina <cris.barbagallo@gmail.com>
2018-12-06 13:03:13 +0100

    Create category object

---
+++ classes/category.php
@@ -0,0 +1,30 @@
+<?php
+
+class Category
+{
+ 
+    // database connection and table name
+    private $pdo;
+ 
+    public function __construct($pdo){
+        $this->pdo = $pdo;
+    }
+ 
+    // used by select drop-down list
+    public function getCategory(){
+        //select all data
+        $statement = $this->pdo->prepare(
+            "SELECT * FROM posts
+            LEFT JOIN post_category ON posts.posts_id = post_category.posts_id
+            LEFT JOIN categories ON categories.categories_id = post_category.category_id
+            ORDER BY posts.created_on DESC"//:category_id
+        );
+
+        $statement->execute();
+        $rows = $statement->fetchAll();
+
+ 
+        return $rows;
+    }
+
+}   
