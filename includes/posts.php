5af4de4b67a4f0871381658c22e0d208618cd8b1
mhedengren <melker.hedengren@medieinstitutet.se>
2018-12-13 11:36:36 +0100

    minor indentation

--- includes/posts.php
+++ includes/posts.php
@@ -18,10 +18,10 @@ $action = $_GET["action"] ?? '';
 // Handles what happens if the GET action is create_post.
 if($action === "create_post")
 { 
-    //Run validation.
-    create_validation();
-    //Call to create method.
-    $posts->create($_POST["postTitle"], $_POST["postDesc"], $_POST["postCont"], $_SESSION["user_id"], date('Y-m-d H:i:s'), $new_location, $_POST["postAlt"], $_POST['categories'] );
+   //Run validation.
+   create_validation();
+   //Call to create method.
+   $posts->create($_POST["postTitle"], $_POST["postDesc"], $_POST["postCont"], $_SESSION["user_id"], date('Y-m-d H:i:s'), $new_location, $_POST["postAlt"], $_POST['categories'] );
 }
 
 // Handles what happens if the GET action is read_post.
@@ -38,14 +38,14 @@ if($action === "update_post")
    //Run validation.
    update_validation();
    //Call to update method
-    $posts->update($_POST["postTitle"], $_POST["postDesc"], $_POST["postCont"], $new_location, $_POST["postAlt"], $_POST['categories'], $postId );
+   $posts->update($_POST["postTitle"], $_POST["postDesc"], $_POST["postCont"], $new_location, $_POST["postAlt"], $_POST['categories'], $postId );
 }
 
 // Handles what happens if the GET action is delete_post.
 if($action === "delete_post")
 { 
-  $id_to_delete = $_GET["id"];
-  $posts->delete($id_to_delete);
+   $id_to_delete = $_GET["id"];
+   $posts->delete($id_to_delete);
 }
 
   
