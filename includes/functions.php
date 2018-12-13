444b7b3d392de627108cec1f1fbcf0027884e185
Al Kanngo <aladdin.kanngo@hotmail.com>
2018-12-13 17:57:29 +0100

    Add/delete spacing/comments on functions.php

--- includes/functions.php
+++ includes/functions.php
@@ -1,12 +1,14 @@
 <?php
 //This function redirects to a specific page
 function redirect_to($location){
+    
     header('Location: ' .$location);
     exit; //exit() should be set if more action is set after this redirect.
 }
 
 //This function redirect if user is not logged in
 function is_login($location){
+
     if(!isset($_SESSION['username'])){
         redirect_to($location);
     }
@@ -14,6 +16,7 @@ function is_login($location){
 
 //This function redirects if user is not logged in as an admin
 function is_admin($location){
+
     if((!isset($_SESSION['user'])) || $_SESSION['user'] == "standard"){
         redirect_to($location);
     }
@@ -21,6 +24,7 @@ function is_admin($location){
 
 //This function displays error messages which are corrected in Class
 function display_errors($errors = array()){
+
     $output = '';
     if(!empty($errors)){
         $output .= "<div class=\"errors\">";
@@ -39,35 +43,37 @@ function display_errors($errors = array()){
   the inputs field in the add new post form */
 function new_post_form_check() {
 
- if(isset($_GET['empty'])) {   
-    if($_GET['empty'] == 'title') {
-        echo "<div class='alert alert-danger text-center' role='alert'>Please insert the title!</div>";
-    } else if($_GET['empty'] == 'description') {
-        echo "<div class='alert alert-danger text-center' role='alert'>Please insert the description!</div>";
-    } else if($_GET['empty'] == 'content') {
-        echo "<div class='alert alert-danger text-center' role='alert'>Please insert the content!</div>";
-    } else if($_GET['empty'] == 'form') {
-        echo "<div class='alert alert-danger text-center' role='alert'>You did not fill in all fields! </div>";
+    if(isset($_GET['empty'])) {   
+        if($_GET['empty'] == 'title') {
+            echo "<div class='alert alert-danger text-center' role='alert'>Please insert the title!</div>";
+        } else if($_GET['empty'] == 'description') {
+            echo "<div class='alert alert-danger text-center' role='alert'>Please insert the description!</div>";
+        } else if($_GET['empty'] == 'content') {
+            echo "<div class='alert alert-danger text-center' role='alert'>Please insert the content!</div>";
+        } else if($_GET['empty'] == 'form') {
+            echo "<div class='alert alert-danger text-center' role='alert'>You did not fill in all fields! </div>";
+        } 
     } 
- } else if (isset($_GET['action'])) {
-     if($_GET['action'] == 'added')
-    echo "<div class='alert alert-success text-center' role='alert'>Your post has been successfully added!</div>";
- }
+    if(isset($_GET['action'])) {
+        if($_GET['action'] == 'added')
+            echo "<div class='alert alert-success text-center' role='alert'>Your post has been successfully added!</div>";
+    }
 }
 
 function new_comment_form_check(){
+
     if(isset($_GET['empty'])){
         if($_GET['empty'] == 'content'){
             echo "<div class='alert alert-danger text-center' role='alert'>Please fill in comment box before posting.</div>";
         }
     } else if (isset($_GET['action'])) {
         if($_GET['action'] == 'added')
-       echo "<div class='alert alert-success text-center' role='alert'>Your comment has been successfully added!</div>";
-}
-
+            echo "<div class='alert alert-success text-center' role='alert'>Your comment has been successfully added!</div>";
+    }
 }
 
 function upload_file_check() {
+
     if(isset($_GET['upload'])) {
         if($_GET['upload'] == 'wrongformat') {
             echo "<div class='alert alert-warning text-center' role='alert'>Fil extension not allowed, please choose a JPEG, JPG or PNG file.</div>";
