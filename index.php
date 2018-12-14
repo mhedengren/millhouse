88ba19b86b80748ace86e9529351772ce0413d46
mhedengren <melker.hedengren@medieinstitutet.se>
2018-12-14 10:29:12 +0100

    minor indentation

--- index.php
+++ index.php
@@ -30,28 +30,30 @@ include 'includes/header.php';
             <?php if(isset($_SESSION['user']) && $_SESSION['user'] == "admin"): ?>
                     <ul class="edit-remove-buttons">
                         <li class="list-inline-item edit-button">
-                        <a href="includes/posts.php?action=read_post&id=<?= $feature["posts_id"]; ?>">
+                            <a href="includes/posts.php?action=read_post&id=<?= $feature["posts_id"]; ?>">
                         <i class="fas fa-pencil-alt"></i></a></li>
                         <li class="list-inline-item remove-button">
-                        <a href="includes/posts.php?action=delete_post&id=<?= $feature["posts_id"]; ?>">
+                            <a href="includes/posts.php?action=delete_post&id=<?= $feature["posts_id"]; ?>">
                         <i class="fas fa-times"></i></a></li>
                     </ul>
             <?php endif; ?>
 
             <a class="no-hover" href="views/single-post.php?posts_id=<?= $feature["posts_id"]; ?>">
+
                 <!-- Hero image -->
                 <div class="hero-image">
-                <img src="includes/<?= $feature["image"]; ?>" alt="<?= $feature["alt"]; ?>">
+                    <img src="includes/<?= $feature["image"]; ?>" alt="<?= $feature["alt"]; ?>">
                 </div>
                 
                 <!-- Mobile feature -->
                 <div class="date row justify-content-center">
-                <div class="date-circle d-md-none">
-                <h6><?= $month; ?><br><?= $day; ?></h6>
-                </div>
+                    <div class="date-circle d-md-none">
+                        <h6><?= $month; ?><br><?= $day; ?></h6>
+                    </div>
                 </div>
                 <h2 class="feature-title-mobile d-md-none"><?= $feature["title"]; ?></h2>
                 <p class="post-description d-md-none"><?= $feature["description"]; ?></p>
+
             </a>
 
         </div>
